8ac3c98c969efdcd8bfffa447c1a2b1d6d4c66ea
Robert Harrison <harrisonrw@gmail.com>
2024-09-24 13:28:27 -0700

    Initial Final Project

---
+++ final-project/Example/Example/ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExampleApp.swift
+//  Example
+//
+//  Created by Robert Harrison on 9/24/24.
+//
+
+import SwiftUI
+
+@main
+struct ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
